340ba6c6b15154a09e060f8b9b7d8c6589905da8
Sandertv <st.ten.veldhuis@gmail.com>
2017-01-28 00:14:52 +0100

    Fix a bunch of bugs, clean up some stuff and make everything look a bit nicer.

--- src/MiniBosses/Boss.php
+++ src/MiniBosses/Boss.php
@@ -2,24 +2,24 @@
 
 namespace MiniBosses;
 
-use pocketmine\Player;
-use pocketmine\utils\UUID;
 use pocketmine\entity\Creature;
-use pocketmine\nbt\tag\ListTag;
-use pocketmine\nbt\tag\IntTag;
-use pocketmine\nbt\tag\FloatTag;
-use pocketmine\nbt\tag\StringTag;
-use pocketmine\nbt\tag\ByteTag;
-use pocketmine\nbt\tag\DoubleTag;
-use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\level\Position;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\item\Item;
+use pocketmine\level\Position;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\StringTag;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\network\protocol\AddPlayerPacket;
+use pocketmine\Player;
+use pocketmine\utils\UUID;
 
-class Boss extends Creature{
+class Boss extends Creature {
 	
+	const NETWORK_ID = 1000;
 	public $networkId = 32;
 	public $target;
 	public $spawnPos;
@@ -33,46 +33,47 @@ class Boss extends Creature{
 	public $heldItem;
 	public $range;
 	public $knockbackTicks = 0;
-	const NETWORK_ID = 1000;
+	public $plugin;
 	
-	public function __construct($chunk,$nbt){
-		parent::__construct($chunk,$nbt);
+	public function __construct($chunk, $nbt) {
+		parent::__construct($chunk, $nbt);
 		$this->networkId = $this->namedtag["networkId"];
 		$this->range = $this->namedtag["range"];
-		$this->spawnPos = new Position($this->namedtag["spawnPos"][0],$this->namedtag["spawnPos"][1],$this->namedtag["spawnPos"][2],$this->level);
+		$this->spawnPos = new Position($this->namedtag["spawnPos"][0], $this->namedtag["spawnPos"][1], $this->namedtag["spawnPos"][2], $this->level);
 		$this->attackDamage = $this->namedtag["attackDamage"];
 		$this->attackRate = $this->namedtag["attackRate"];
 		$this->speed = $this->namedtag["speed"];
-		foreach(explode(' ',$this->namedtag["drops"]) as $item){
-			$item = explode(';',$item);
-			$this->drops[] = Item::get($item[0],isset($item[1]) ? $item[1] : 0,isset($item[2]) ? $item[2] : 1,isset($item[3]) ? $item[3] : "");
+		foreach(explode(' ', $this->namedtag["drops"]) as $item) {
+			$item = explode(';', $item);
+			$this->drops[] = Item::get($item[0], isset($item[1]) ? $item[1] : 0, isset($item[2]) ? $item[2] : 1, isset($item[3]) ? $item[3] : "");
 		}
 		$this->respawnTime = $this->namedtag["respawnTime"];
 		$this->skin = $this->namedtag["skin"];
-		$heldItem = explode(';',$this->namedtag["heldItem"]);
-		$this->heldItem = Item::get($heldItem[0],isset($heldItem[1]) ? $heldItem[1] : 0,isset($heldItem[2]) ? $heldItem[2] : 1,isset($heldItem[3]) ? $heldItem[3] : "");
+		$heldItem = explode(';', $this->namedtag["heldItem"]);
+		$this->heldItem = Item::get($heldItem[0], isset($heldItem[1]) ? $heldItem[1] : 0, isset($heldItem[2]) ? $heldItem[2] : 1, isset($heldItem[3]) ? $heldItem[3] : "");
 	}
 	
-	public function initEntity(){
+	public function initEntity() {
 		$this->plugin = $this->server->getPluginManager()->getPlugin("MiniBosses");
-        parent::initEntity();
-        //$this->dataProperties[self::DATA_NO_AI] = [self::DATA_TYPE_BYTE, 1];
-		if(isset($this->namedtag->maxHealth)){
+		parent::initEntity();
+		$this->dataProperties[self::DATA_FLAG_NO_AI] = [self::DATA_TYPE_BYTE, 1];
+		if(isset($this->namedtag->maxHealth)) {
 			parent::setMaxHealth($this->namedtag["maxHealth"]);
 			$this->setHealth($this->namedtag["maxHealth"]);
-		}else{
+		} else {
 			$this->setMaxHealth(20);
 			$this->setHealth(20);
 		}
-    }
-	
-	public function getName(){
-		return $this->getNameTag();
 	}
 	
-	public function spawnTo(Player $player){
+	public function setMaxHealth($health) {
+		$this->namedtag->maxHealth = new IntTag("maxHealth", $health);
+		parent::setMaxHealth($health);
+	}
+	
+	public function spawnTo(Player $player) {
 		parent::spawnTo($player);
-		if($this->networkId === 63){
+		if($this->networkId === 63) {
 			$pk = new AddPlayerPacket();
 			$pk->uuid = UUID::fromData($this->getId(), $this->skin, $this->getNameTag());
 			$pk->username = $this->getName();
@@ -88,7 +89,7 @@ class Boss extends Creature{
 			$pk->item = $this->heldItem;
 			$pk->metadata = $this->dataProperties;
 			$player->dataPacket($pk);
-		}else{
+		} else {
 			$pk = new AddEntityPacket();
 			$pk->eid = $this->getID();
 			$pk->type = $this->networkId;
@@ -105,65 +106,67 @@ class Boss extends Creature{
 		}
 	}
 	
-	public function setMaxHealth($health){
-		$this->namedtag->maxHealth = new IntTag("maxHealth",$health);
-		parent::setMaxHealth($health);
+	public function getName() {
+		return $this->getNameTag();
 	}
 	
-	public function saveNBT(){
-        parent::saveNBT();
-		$this->namedtag->maxHealth = new IntTag("maxHealth",$this->getMaxHealth());
+	public function saveNBT() {
+		parent::saveNBT();
+		$this->namedtag->maxHealth = new IntTag("maxHealth", $this->getMaxHealth());
 		$this->namedtag->spawnPos = new ListTag("spawnPos", [
-                new DoubleTag("", $this->spawnPos->x),
-                new DoubleTag("", $this->spawnPos->y),
-                new DoubleTag("", $this->spawnPos->z)
-            ]);
-		$this->namedtag->range = new FloatTag("range",$this->range);
-		$this->namedtag->attackDamage = new FloatTag("attackDamage",$this->attackDamage);
-		$this->namedtag->networkId = new IntTag("networkId",$this->networkId);
-		$this->namedtag->attackRate = new IntTag("attackRate",$this->attackRate);
-		$this->namedtag->speed = new FloatTag("speed",$this->speed);
+			new DoubleTag("", $this->spawnPos->x),
+			new DoubleTag("", $this->spawnPos->y),
+			new DoubleTag("", $this->spawnPos->z)
+		]);
+		$this->namedtag->range = new FloatTag("range", $this->range);
+		$this->namedtag->attackDamage = new FloatTag("attackDamage", $this->attackDamage);
+		$this->namedtag->networkId = new IntTag("networkId", $this->networkId);
+		$this->namedtag->attackRate = new IntTag("attackRate", $this->attackRate);
+		$this->namedtag->speed = new FloatTag("speed", $this->speed);
 		$drops2 = [];
-		foreach($this->drops as $drop) $drops2[] = $drop->getId().";".$drop->getDamage().";".$drop->getCount().";".$drop->getCompoundTag();
-		$this->namedtag->drops = new StringTag("drops",implode(' ',$drops2));
-		$this->namedtag->respawnTime = new IntTag("respawnTime",$this->respawnTime);
-		$this->namedtag->skin = new StringTag("skin",$this->skin);
-		$this->namedtag->heldItem = new StringTag("heldItem",($this->heldItem instanceof Item ? $this->heldItem->getId().";".$this->heldItem->getDamage().";".$this->heldItem->getCount().";".$this->heldItem->getCompoundTag() : ""));
-    }
+		foreach($this->drops as $drop)
+			$drops2[] = $drop->getId() . ";" . $drop->getDamage() . ";" . $drop->getCount() . ";" . $drop->getCompoundTag();
+		$this->namedtag->drops = new StringTag("drops", implode(' ', $drops2));
+		$this->namedtag->respawnTime = new IntTag("respawnTime", $this->respawnTime);
+		$this->namedtag->skin = new StringTag("skin", $this->skin);
+		$this->namedtag->heldItem = new StringTag("heldItem", ($this->heldItem instanceof Item ? $this->heldItem->getId() . ";" . $this->heldItem->getDamage() . ";" . $this->heldItem->getCount() . ";" . $this->heldItem->getCompoundTag() : ""));
+	}
 	
-	public function onUpdate($currentTick){
-		if($this->knockbackTicks > 0) $this->knockbackTicks--;
-		if(($player = $this->target) && $player->isAlive()){
-			if($this->distanceSquared($this->spawnPos) > $this->range){
+	public function onUpdate($currentTick) {
+		if($this->knockbackTicks > 0) {
+			$this->knockbackTicks--;
+		}
+		if(($player = $this->target) && $player->isAlive()) {
+			if($this->distanceSquared($this->spawnPos) > $this->range) {
 				$this->setPosition($this->spawnPos);
 				$this->setHealth($this->getMaxHealth());
 				$this->target = null;
-			}else{
-				if(!$this->onGround){
-					if($this->motionY > -$this->gravity * 4){
+			} else {
+				if(!$this->onGround) {
+					if($this->motionY > -$this->gravity * 4) {
 						$this->motionY = -$this->gravity * 4;
-					}else{
+					} else {
 						$this->motionY -= $this->gravity;
 					}
 					$this->move($this->motionX, $this->motionY, $this->motionZ);
-				}elseif($this->knockbackTicks > 0){
+				} elseif($this->knockbackTicks > 0) {
 					
-				}else{
-					$x = $player-> x - $this->x;
-					$y = $player-> y - $this->y;
-					$z = $player-> z - $this->z;
-					if($x ** 2 + $z ** 2 < 0.7){
+				} else {
+					$x = $player->x - $this->x;
+					$y = $player->y - $this->y;
+					$z = $player->z - $this->z;
+					if($x ** 2 + $z ** 2 < 0.7) {
 						$this->motionX = 0;
 						$this->motionZ = 0;
-					}else{
+					} else {
 						$diff = abs($x) + abs($z);
 						$this->motionX = $this->speed * 0.15 * ($x / $diff);
 						$this->motionZ = $this->speed * 0.15 * ($z / $diff);
 					}
-					$this->yaw = rad2deg(atan2(-$x,$z));
+					$this->yaw = rad2deg(atan2(-$x, $z));
 					$this->pitch = rad2deg(atan(-$y));
 					$this->move($this->motionX, $this->motionY, $this->motionZ);
-					if($this->distanceSquared($this->target) < 1 && $this->attackDelay++ > $this->attackRate){
+					if($this->distanceSquared($this->target) < $this->range && $this->attackDelay++ > $this->attackRate) {
 						$this->attackDelay = 0;
 						$ev = new EntityDamageByEntityEvent($this, $this->target, EntityDamageEvent::CAUSE_ENTITY_ATTACK, $this->attackDamage);
 						$player->attack($ev->getFinalDamage(), $ev);
@@ -176,10 +179,10 @@ class Boss extends Creature{
 		return !$this->closed;
 	}
 	
-	public function attack($damage, EntityDamageEvent $source){
-		if(!$source->isCancelled() && $source instanceof EntityDamageByEntityEvent){
+	public function attack($damage, EntityDamageEvent $source) {
+		if(!$source->isCancelled() && $source instanceof EntityDamageByEntityEvent) {
 			$dmg = $source->getDamager();
-			if($dmg instanceof Player){
+			if($dmg instanceof Player) {
 				$this->target = $dmg;
 				parent::attack($damage, $source);
 				$this->motionX = ($this->x - $dmg->x) * 0.19;
@@ -187,15 +190,15 @@ class Boss extends Creature{
 				$this->motionZ = ($this->z - $dmg->z) * 0.19;
 				$this->knockbackTicks = 10;
 			}
-        }
-    }
-	
-	public function kill(){
-		parent::kill();
-		$this->plugin->respawn($this->getNameTag(),$this->respawnTime);
+		}
 	}
 	
-	public function getDrops(){
+	public function kill() {
+		parent::kill();
+		$this->plugin->respawn($this->getNameTag(), $this->respawnTime);
+	}
+	
+	public function getDrops() {
 		return $this->drops;
 	}
 }
--- src/MiniBosses/Main.php
+++ src/MiniBosses/Main.php
@@ -2,139 +2,157 @@
 
 namespace MiniBosses;
 
-use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\level\Level;
-use pocketmine\scheduler\PluginTask;
-use pocketmine\utils\TextFormat;
-use pocketmine\event\Listener;
-use pocketmine\utils\Config;
-use pocketmine\entity\Effect;
-use pocketmine\entity\Entity;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\DoubleTag;
-use pocketmine\nbt\tag\ListTag;
-use pocketmine\nbt\tag\IntTag;
-use pocketmine\nbt\tag\FloatTag;
-use pocketmine\nbt\tag\StringTag;
-use pocketmine\level\Location;
-use pocketmine\level\Position;
-use pocketmine\nbt\NBT;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\entity\Entity;
+use pocketmine\event\Listener;
+use pocketmine\level\Position;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TF;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener {
 	
-	const NetworkIds = array("chicken"=>10,
-							"cow"=>11,
-							"pig"=>12,
-							"sheep"=>13,
-							"wolf"=>14,
-							"villager"=>15,
-							"mooshroom"=>16,
-							"squid"=>17,
-							"rabbit"=>18,
-							"bat"=>19,
-							"irongolem"=>20,
-							"snowgolem"=>21,
-							"ocelot"=>22,
-							"horse"=>23,
-							"donkey"=>24,
-							"mule"=>25,
-							"skeletonhorse"=>26,
-							"zombiehorse"=>27,
-							"zombie"=>32,
-							"creeper"=>33,
-							"skeleton"=>34,
-							"spider"=>35,
-							"pigman"=>36,
-							"slime"=>37,
-							"enderman"=>38,
-							"silverfish"=>39,
-							"cavespider"=>40,
-							"magmacube"=>42,
-							"blaze"=>43,
-							"zombievillager"=>44,
-							"witch"=>45,
-							"stray"=>46,
-							"husk"=>47,
-							"witherskeleton"=>48,
-							"human"=>63);
-
-	public function onEnable(){
+	const NetworkIds = [
+		"chicken" => 10,
+		"cow" => 11,
+		"pig" => 12,
+		"sheep" => 13,
+		"wolf" => 14,
+		"villager" => 15,
+		"mooshroom" => 16,
+		"squid" => 17,
+		"rabbit" => 18,
+		"bat" => 19,
+		"irongolem" => 20,
+		"snowgolem" => 21,
+		"ocelot" => 22,
+		"horse" => 23,
+		"donkey" => 24,
+		"mule" => 25,
+		"skeletonhorse" => 26,
+		"zombiehorse" => 27,
+		"zombie" => 32,
+		"creeper" => 33,
+		"skeleton" => 34,
+		"spider" => 35,
+		"pigman" => 36,
+		"slime" => 37,
+		"enderman" => 38,
+		"silverfish" => 39,
+		"cavespider" => 40,
+		"magmacube" => 42,
+		"blaze" => 43,
+		"zombievillager" => 44,
+		"witch" => 45,
+		"stray" => 46,
+		"husk" => 47,
+		"witherskeleton" => 48,
+		"human" => 63
+	];
+	
+	public function onEnable() {
 		@mkdir($this->getDataFolder());
 		Entity::registerEntity(Boss::class);
-		$this->data = new Config($this->getDataFolder()."Bosses.yml",Config::YAML);
+		$this->data = new Config($this->getDataFolder() . "Bosses.yml", Config::YAML);
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 	
-	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-		if(!isset($args[0])){
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+		if(!isset($args[0])) {
 			$sender->sendMessage("Usage: /minibosses create/spawn/delete/list");
-		}elseif($args[0] === "create"){
-			if(!($sender instanceof Player)) $sender->sendMessage("Please run in-game");
-			elseif(count($args) >= 3){
+		} elseif($args[0] === "create") {
+			if(!($sender instanceof Player)) {
+				$sender->sendMessage("Please run in-game");
+			} elseif(count($args) >= 3) {
 				$networkid = $args[1];
 				array_shift($args);
 				array_shift($args);
-				$name = implode(' ',$args);
-				if($this->data->get($name,null) === null){
-					if(($search = array_search($networkid,self::NetworkIds,true)) === false && !isset(self::NetworkIds[strtolower($networkid)])){
-						$sender->sendMessage("Unrecognised Network ID or Entity type $networkid");
-						return true;
+				$name = implode(' ', $args);
+				if($this->data->get($name, null) === null) {
+					if(($search = array_search($networkid, self::NetworkIds, true)) !== false){
+						if($search === false) {
+							$networkid = self::NetworkIds[strtolower($networkid)];
+						}
 					}else{
-						if($search === false) $networkid = self::NetworkIds[strtolower($networkid)];
+						$sender->sendMessage(TF::RED . "Unrecognised Network ID or Entity type $networkid");
+						return true;
 					}
 					$heldItem = $sender->getInventory()->getItemInHand();
-					$this->data->set($name,array("network-id" => $networkid,"x"=>$sender->x,"y"=>$sender->y,"z"=>$sender->z,"level"=>$sender->level->getName(),"health"=>20,"range"=>10,"attackDamage"=>1,"attackRate"=>10,"speed"=>1,"drops"=>"1;2;3 4;5;6 7;8;9","respawnTime"=>100,"skin"=>($networkid === 63 ? bin2hex($sender->getSkinData()) : ""),"heldItem"=>($networkid === 63 ? $heldItem->getId().";".$heldItem->getDamage().";".$heldItem->getCount().";" : "")));
+					$this->data->set($name, array("network-id" => $networkid, "x" => $sender->x, "y" => $sender->y, "z" => $sender->z, "level" => $sender->level->getName(), "health" => 20, "range" => 10, "attackDamage" => 1, "attackRate" => 10, "speed" => 1, "drops" => "1;2;3 4;5;6 7;8;9", "respawnTime" => 100, "skin" => ($networkid === 63 ? bin2hex($sender->getSkinData()) : ""), "heldItem" => ($networkid === 63 ? $heldItem->getId() . ";" . $heldItem->getDamage() . ";" . $heldItem->getCount() . ";" : "")));
 					$this->data->save();
 					$this->spawnBoss($name);
-					$sender->sendMessage("Successfully created $name");
-				}else $sender->sendMessage("Already exists");
-			}else $sender->sendMessage("Usage: /minibosses create network-id name");
-		}elseif($args[0] === "spawn"){
-			if(count($args) >= 2){
+					$sender->sendMessage(TF::GREEN . "Successfully created $name");
+				} else {
+					$sender->sendMessage(TF::RED . "That MiniBoss already exists");
+				}
+			} else {
+				$sender->sendMessage(TF::RED . "Usage: /minibosses create network-id name");
+			}
+		} elseif($args[0] === "spawn") {
+			if(count($args) >= 2) {
 				array_shift($args);
-				$name = implode(' ',$args);
-				if($this->data->get($name,null) !== null){
+				$name = implode(' ', $args);
+				if($this->data->get($name, null) !== null) {
 					$ret = $this->spawnBoss($name);
-					if($ret === true) $sender->sendMessage("Successfully spawned $name");
-					else $sender->sendMessage("Error spawning $name : $ret");
-				}else $sender->sendMessage("Not exist");
-			}else $sender->sendMessage("Usage: /minibosses spawn name");
-		}elseif($args[0] === "delete"){
-			if(count($args) >= 2){
+					if($ret === true) {
+						$sender->sendMessage(TF::GREEN . "Successfully spawned MiniBoss: $name");
+					} else {
+						$sender->sendMessage(TF::RED . "Error spawning MiniBoss: $name : $ret");
+					}
+				} else {
+					$sender->sendMessage(TF::RED . "That MiniBoss does not exist!");
+				}
+			} else {
+				$sender->sendMessage(TF::RED . "Usage: /minibosses spawn name");
+			}
+		} elseif($args[0] === "delete") {
+			if(count($args) >= 2) {
 				array_shift($args);
 				$name = implode($args);
-				if($this->data->get($name,null) !== null){
+				if($this->data->get($name, null) !== null) {
 					$this->data->remove($name);
 					$this->data->save();
 					$sender->sendMessage("Successfully removed $name");
-					foreach($this->getServer()->getLevels() as $l){
-						foreach($l->getEntities() as $e){
-							if($e instanceof Boss && $e->getNameTag() === $name) $e->close();
+					foreach($this->getServer()->getLevels() as $l) {
+						foreach($l->getEntities() as $e) {
+							if($e instanceof Boss && $e->getNameTag() === $name) {
+								$e->close();
+							}
 						}
 					}
-				}else $sender->sendMessage("Not exist");
-			}else $sender->sendMessage("Usage: /minibosses delete name");
-		}elseif($args[0] === "list"){
-			$sender->sendMessage("----MiniBosses----");
-			$sender->sendMessage(implode(', ',array_keys($this->data->getAll())));
-		}else{
-			$sender->sendMessage("Usage: /minibosses create/spawn/delete/list");
+				} else {
+					$sender->sendMessage(TF::RED . "That MiniBoss does not exist!");
+				}
+			} else {
+				$sender->sendMessage(TF::RED . "Usage: /minibosses delete name");
+			}
+		} elseif($args[0] === "list") {
+			$sender->sendMessage(TF::GREEN . "----MiniBosses----");
+			$sender->sendMessage(implode(', ', array_keys($this->data->getAll())));
+		} else {
+			$sender->sendMessage(TF::RED . "Usage: /minibosses create/spawn/delete/list");
 		}
 		return true;
 	}
 	
-	public function spawnBoss(string $name = "Boss"){
+	public function spawnBoss(string $name = "Boss") {
 		$data = $this->data->get($name);
-		if(!$data) return "No data, Boss does not exist";
-		elseif(!$this->getServer()->getLevelByName($data["level"]) && !$this->getServer()->loadLevel($data["level"]))
+		if(!$data) {
+			return "No data, Boss does not exist";
+		} elseif(!$this->getServer()->getLevelByName($data["level"]) && !$this->getServer()->loadLevel($data["level"])) {
 			return "Failed to load Level {$data["level"]}";
+		}
 		$networkId = $data["network-id"];
-		$pos = new Position($data["x"],$data["y"],$data["z"],$this->getServer()->getLevelByName($data["level"]));
+		$pos = new Position($data["x"], $data["y"], $data["z"], $this->getServer()->getLevelByName($data["level"]));
 		$health = $data["health"];
 		$range = $data["health"];
 		$attackDamage = $data["attackDamage"];
@@ -145,56 +163,60 @@ class Main extends PluginBase implements Listener{
 		$skin = ($networkId === 63 ? $data["skin"] : "");
 		$heldItem = ($networkId === 63 ? $data["heldItem"] : "");
 		$nbt = new CompoundTag("", [
-            "Pos" => new ListTag("Pos", [
-                new DoubleTag("", $pos->x),
-                new DoubleTag("", $pos->y),
-                new DoubleTag("", $pos->z)
-            ]),
-            "Motion" => new ListTag("Motion", [
-                new DoubleTag("", 0),
-                new DoubleTag("", 0),
-                new DoubleTag("", 0)
-            ]),
-            "Rotation" => new ListTag("Rotation", [
-                new FloatTag("", 0),
-                new FloatTag("", 0)
-            ]),
+			"Pos" => new ListTag("Pos", [
+				new DoubleTag("", $pos->x),
+				new DoubleTag("", $pos->y),
+				new DoubleTag("", $pos->z)
+			]),
+			"Motion" => new ListTag("Motion", [
+				new DoubleTag("", 0),
+				new DoubleTag("", 0),
+				new DoubleTag("", 0)
+			]),
+			"Rotation" => new ListTag("Rotation", [
+				new FloatTag("", 0),
+				new FloatTag("", 0)
+			]),
 			"spawnPos" => new ListTag("spawnPos", [
-                new DoubleTag("", $pos->x),
-                new DoubleTag("", $pos->y),
-                new DoubleTag("", $pos->z)
-            ]),
-			"range" => new FloatTag("range",$range * $range),
-			"attackDamage" => new FloatTag("attackDamage",$attackDamage),
-			"networkId" => new IntTag("networkId",$networkId),
-			"attackRate" => new IntTag("attackRate",$attackRate),
-			"speed" => new FloatTag("speed",$speed),
-			"drops" => new StringTag("drops",$drops),
-			"respawnTime" => new IntTag("respawnTime",$respawnTime),
-			"skin" => new StringTag("skin",$skin),
-			"heldItem" => new StringTag("heldItem",$heldItem)
-            ]);
-		$ent = Entity::createEntity("Boss",$pos->level->getChunk($pos->x >> 4,$pos->z >> 4,true),$nbt);
+				new DoubleTag("", $pos->x),
+				new DoubleTag("", $pos->y),
+				new DoubleTag("", $pos->z)
+			]),
+			"range" => new FloatTag("range", $range * $range),
+			"attackDamage" => new FloatTag("attackDamage", $attackDamage),
+			"networkId" => new IntTag("networkId", $networkId),
+			"attackRate" => new IntTag("attackRate", $attackRate),
+			"speed" => new FloatTag("speed", $speed),
+			"drops" => new StringTag("drops", $drops),
+			"respawnTime" => new IntTag("respawnTime", $respawnTime),
+			"skin" => new StringTag("skin", $skin),
+			"heldItem" => new StringTag("heldItem", $heldItem)
+		]);
+		$ent = Entity::createEntity("Boss", $pos->level->getChunk($pos->x >> 4, $pos->z >> 4, true), $nbt);
 		$ent->setMaxHealth($health);
 		$ent->setHealth($health);
 		$ent->setNameTag($name);
+		$ent->setNameTagAlwaysVisible(true);
 		$ent->spawnToAll();
 		return true;
 	}
 	
-	public function respawn($name,$time){
-		if($this->data->get($name)) $this->getServer()->getScheduler()->scheduleDelayedTask(new RespawnTask($this,$name), $time);
+	public function respawn($name, $time) {
+		if($this->data->get($name)) {
+			$this->getServer()->getScheduler()->scheduleDelayedTask(new RespawnTask($this, $name), $time);
+		}
 	}
 }
-class RespawnTask extends PluginTask{
+
+class RespawnTask extends PluginTask {
 	
-	public function __construct(Main $plugin,$name){
+	public function __construct(Main $plugin, $name) {
 		parent::__construct($plugin);
 		$this->plugin = $plugin;
 		$this->name = $name;
 	}
 	
-	public function onRun($currentTick){
+	public function onRun($currentTick) {
 		$this->plugin->spawnBoss($this->name);
 	}
 }
